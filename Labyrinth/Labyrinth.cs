0da1c6c008331b85950863381aa19df0b79546f2
SezSahin <sezg0006@edu.eal.dk>
2017-10-14 19:18:49 +0200

    Add project files.

---
+++ Labyrinth/Labyrinth.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth
+{
+    public class Labyrinth //: ILabyrinth
+    {
+        public bool currentPlayer = true;
+
+        public char[,] Board;
+
+        //public string GetBoardView()
+        //{
+        //    Board = new char[10, 10]
+        //    {
+        //        { }
+        //    }
+        //}
+        public bool Move()
+        {
+            int col = 7;
+            int row = 6;
+            Console.SetCursorPosition(col, row);
+            Console.Write("*");
+            Console.SetCursorPosition(col, row);
+            while (true)
+            {
+                ConsoleKeyInfo info = Console.ReadKey(true);
+                if (info.Key == ConsoleKey.W)
+                {
+                    Console.Write(" ");
+                    Debug.Print("W");
+                    row--;
+                }
+                if (info.Key == ConsoleKey.S)
+                {
+                    Console.Write(" ");
+                    Debug.Print("S");
+                    row++;
+                }
+
+                if (info.Key == ConsoleKey.A)
+                {
+                    Console.Write(" ");
+                    Debug.Print("A");
+                    col--;
+                }
+
+                if (info.Key == ConsoleKey.D)
+                {
+                    Console.Write(" ");
+                    Debug.Print("D");
+                    col++;
+                }
+
+                Console.SetCursorPosition(col, row);
+                Console.Write("*");
+                Console.SetCursorPosition(col, row);
+            }
+        }
+        public bool CheckWin()
+        {
+            return true;
+        }
+    }
+}
---
+++ Labyrinth/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Labyrinth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            Console.WriteLine("Welcome to Labyrinth  \n\nDo you wish to play?  ");
+            Thread.Sleep(70);
+            Console.WriteLine();
+            switch (Console.ReadLine())
+            {
+                case "Yes":
+                    myProgram.Labyrinth();
+                    break;
+                case "yes":
+                    myProgram.Labyrinth();
+                    break;
+                case "No":
+                    break;
+                case "no":
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+        private void Labyrinth()
+        {
+            Labyrinth Lab = new Labyrinth();
+            Lab.Move();
+        }
+    }
+}
